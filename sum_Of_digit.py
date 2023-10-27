99aefd9574db84f9915b8194c62cc32d70af19d3
Darwinian_Living_Machine2000 <118555211+Sila2000@users.noreply.github.com>
2023-10-28 00:01:21 +0530

    Update sum_Of_digit.py

--- sum_Of_digit.py
+++ sum_Of_digit.py
@@ -1,6 +1,6 @@
 #author Sila2000
-print("Sum of digits in number:")
-num = o_num = int(input("Enter a non-negative integer: "))
+print("Sum of digits in number (Non-negative Integer):")
+num = o_num = int(input("Enter number: "))
 
 sum_digit = 0
 
